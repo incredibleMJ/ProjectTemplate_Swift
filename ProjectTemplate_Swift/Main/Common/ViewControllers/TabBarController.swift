684e8b2539c22524256f79ced77e58fd6832b81d
chengyingzhan <albert.cheng@pdabc.com>
2019-08-07 12:41:30 +0800

    横竖屏适配

---
+++ ProjectTemplate_Swift/Main/Common/ViewControllers/TabBarController.swift
@@ -0,0 +1,56 @@
+//
+//  TabBarController.swift
+//  Coin2Coin
+//
+//  Created by Albert on 2017/12/12.
+//  Copyright © 2017年 Albert. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    //MARK: - override
+    
+    override var shouldAutorotate: Bool {
+        return selectedViewController?.shouldAutorotate ?? true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return selectedViewController?.supportedInterfaceOrientations ?? .portrait
+    }
+    
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return selectedViewController?.preferredInterfaceOrientationForPresentation ?? .portrait
+    }
+    
+    override var childViewControllerForStatusBarStyle: UIViewController? {
+        return selectedViewController
+    }
+    
+    override var childViewControllerForStatusBarHidden: UIViewController? {
+        return selectedViewController
+    }
+    
+    override func prefersHomeIndicatorAutoHidden() -> Bool {
+        return selectedViewController?.prefersHomeIndicatorAutoHidden() ?? false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func addChildViewController(withVC vc: UIViewController,
+                                title: String,
+                                imageName: String,
+                                selectedImageName: String) {
+        vc.title = title
+        vc.tabBarItem.title = title
+        vc.tabBarItem.image = UIImage.init(named: imageName)?.withRenderingMode(.alwaysOriginal)
+        vc.tabBarItem.selectedImage = UIImage.init(named: selectedImageName)?.withRenderingMode(.alwaysOriginal)
+        
+        let navi = UINavigationController.init(rootViewController: vc)
+        self.addChildViewController(navi)
+    }
+
+}
